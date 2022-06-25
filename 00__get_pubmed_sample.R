4540968e3b8a67646dca8470b4a5fb11613fd8fa
Wim Otte <wmotte@gmail.com>
2022-06-25 09:44:38 +0200

    init

---
+++ 00__get_pubmed_sample.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+#
+# Wim Otte
+#
+# Get sample from PubMed to use for AI.
+########################################
+
+# 155,863 results on 25 June 2022
+query <- 'all[sb] AND 2022/01/01:2022/01/01[dp] AND hasabstract'
