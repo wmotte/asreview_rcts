ac572567d5a061ae0e319530cd859f22cdda6f90
Wim Otte <wmotte@gmail.com>
2022-07-13 11:00:48 +0200

    Protocols added

--- 01__compare_wim_christiaan.R
+++ 01__compare_wim_christiaan.R
@@ -74,9 +74,16 @@ m_final <- get_kappa_data( type = 'meta-analysis-only' )
 s_final <- get_kappa_data( type = 'systematic-reviews-only' )
 r_final <- get_kappa_data( type = 'rodent-studies-only' )
 h_final <- get_kappa_data( type = 'human-studies-only' )
+p_final <- get_kappa_data( type = 'protocols-only' )
+
+# TODO
+#rt_final <- get_kappa_data( type = 'randomised-trials' )
+#c_final <- get_kappa_data( type = 'children-only' )
+
+
 
 # combine
-res <- rbind( m_final$res, s_final$res, r_final$res, h_final$res )
+res <- rbind( m_final$res, s_final$res, r_final$res, h_final$res, p_final$res )
 
 # write to file
 readr::write_tsv( res, file = paste0( outdir, '/kappas_w_c.tsv' ) )
@@ -94,11 +101,15 @@ dim( disagreements_r <- r_final$out[ r_final$out$included_c != r_final$out$inclu
 
 # n = 27 disagreements
 dim( disagreements_h <- h_final$out[ h_final$out$included_c != h_final$out$included_w, ] )
+disagreements_h$Unnamed..0 <- NULL
 
+# n = 4 disagreements
+dim( disagreements_p <- p_final$out[ p_final$out$included_c != p_final$out$included_w, ] )
 
 # write to file
 readr::write_tsv( disagreements_m, file = paste0( outdir, '/disagreements_w_c__meta-analysis.tsv' ) )
 readr::write_tsv( disagreements_s, file = paste0( outdir, '/disagreements_w_c__systematic-reviews.tsv' ) )
 readr::write_tsv( disagreements_r, file = paste0( outdir, '/disagreements_w_c__rodents.tsv' ) )
 readr::write_tsv( disagreements_h, file = paste0( outdir, '/disagreements_w_c__human.tsv' ) )
+readr::write_tsv( disagreements_p, file = paste0( outdir, '/disagreements_w_c__protocols.tsv' ) )
 
