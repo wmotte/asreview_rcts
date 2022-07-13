50f3f08d5fe8b43a7381651a8a295c79cfa42f34
Wim Otte <wmotte@gmail.com>
2022-07-13 16:06:32 +0200

    second round, partially

--- 03__second_round_compare_wim_christiaan.R
+++ 03__second_round_compare_wim_christiaan.R
@@ -1,5 +1,6 @@
 # kappa
-
+# second round
+################################################################################
 
 ###
 # Get kappa
@@ -27,8 +28,8 @@ get_kappa <- function( response, pred_bin )
 get_kappa_data <- function( type )
 {
     # infiles
-    infile_c <- paste0( 'data.christiaan/labeled_data/dataset_', type, '.csv' )
-    infile_w <- paste0( 'data.wim/labeled_data/dataset_', type, '.csv' )
+    infile_c <- paste0( 'data.christiaan/second_round/dataset_', type, '.csv' )
+    infile_w <- paste0( 'data.wim/second_round/dataset_', type, '.csv' )
     
     # get data
     df_c <- read.csv( infile_c )
@@ -64,21 +65,23 @@ get_kappa_data <- function( type )
 ################################################################################
 
 # outdir
-outdir <- 'out.01.compare'
+outdir <- 'out.03.second.round.compare'
 dir.create( outdir, showWarnings = FALSE )
 
-s_final <- get_kappa_data( type = 'systematic-reviews-only' )
-m_final <- get_kappa_data( type = 'meta-analysis-only' )
-r_final <- get_kappa_data( type = 'rodent-studies-only' )
-h_final <- get_kappa_data( type = 'human-studies-only' )
-t_final <- get_kappa_data( type = 'trials-only' )
-rt_final <- get_kappa_data( type = 'randomised-trials-only' )
-c_final <- get_kappa_data( type = 'children-only' )
-p_final <- get_kappa_data( type = 'protocols-only' )
+#s_final <- get_kappa_data( type = '1-round-ii-systematic-reviews' )
+m_final <- get_kappa_data( type = '2-round-ii-meta-analysis' )
+r_final <- get_kappa_data( type = '3-round-ii-rodents' )
+h_final <- get_kappa_data( type = '4-round-ii-humans' )
+#t_final <- get_kappa_data( type = '5-round-ii-trials' )
+rt_final <- get_kappa_data( type = '6-round-ii-randomised-trials' )
+c_final <- get_kappa_data( type = '7-round-ii-children' )
+p_final <- get_kappa_data( type = '8-round-ii-protocols' )
 
 # combine
-res <- rbind( s_final$res, m_final$res, r_final$res, h_final$res, 
-              t_final$res, rt_final$res, c_final$res, p_final$res )
+res <- rbind( #s_final$res, 
+                m_final$res, r_final$res, h_final$res, 
+                #t_final$res, 
+                rt_final$res, c_final$res, p_final$res )
 
 # write to file
 readr::write_tsv( res, file = paste0( outdir, '/kappas_w_c.tsv' ) )
@@ -87,37 +90,37 @@ readr::write_tsv( res, file = paste0( outdir, '/kappas_w_c.tsv' ) )
 # write disagreements to file
 ##############################
 
-# 1) n = 32 disagreements
-dim( disagreements_s <- s_final$out[ s_final$out$included_c != s_final$out$included_w, ] )
+# 1) 
+#dim( disagreements_s <- s_final$out[ s_final$out$included_c != s_final$out$included_w, ] )
 
-# 2) n = 7 disagreements
+# 2) 
 dim( disagreements_m <- m_final$out[ m_final$out$included_c != m_final$out$included_w, ] )
 
-# 3) n = 46 disagreements
+# 3) 
 dim( disagreements_r <- r_final$out[ r_final$out$included_c != r_final$out$included_w, ] )
 
-# 4) n = 27 disagreements
+# 4) 
 dim( disagreements_h <- h_final$out[ h_final$out$included_c != h_final$out$included_w, ] )
 
-# 5) n = 91 disagreements
-dim( disagreements_t <- t_final$out[ t_final$out$included_c != t_final$out$included_w, ] )
+# 5) 
+#dim( disagreements_t <- t_final$out[ t_final$out$included_c != t_final$out$included_w, ] )
 
-# 6) n = 14 disagreements
+# 6) 
 dim( disagreements_rt <- rt_final$out[ rt_final$out$included_c != rt_final$out$included_w, ] )
 
-# 7) n = 66 disagreements
+# 7) 
 dim( disagreements_c <- c_final$out[ c_final$out$included_c != c_final$out$included_w, ] )
 
-# 8) n = 4 disagreements
+# 8) 
 dim( disagreements_p <- p_final$out[ p_final$out$included_c != p_final$out$included_w, ] )
 
 
 # write to file
-readr::write_tsv( disagreements_s, file = paste0( outdir, '/disagreements_w_c__systematic-reviews.tsv' ) )
+#readr::write_tsv( disagreements_s, file = paste0( outdir, '/disagreements_w_c__systematic-reviews.tsv' ) )
 readr::write_tsv( disagreements_m, file = paste0( outdir, '/disagreements_w_c__meta-analysis.tsv' ) )
 readr::write_tsv( disagreements_r, file = paste0( outdir, '/disagreements_w_c__rodents.tsv' ) )
 readr::write_tsv( disagreements_h, file = paste0( outdir, '/disagreements_w_c__human.tsv' ) )
-readr::write_tsv( disagreements_t, file = paste0( outdir, '/disagreements_w_c__trials.tsv' ) )
+#readr::write_tsv( disagreements_t, file = paste0( outdir, '/disagreements_w_c__trials.tsv' ) )
 readr::write_tsv( disagreements_rt, file = paste0( outdir, '/disagreements_w_c__randomised-trials.tsv' ) )
 readr::write_tsv( disagreements_c, file = paste0( outdir, '/disagreements_w_c__child.tsv' ) )
 readr::write_tsv( disagreements_p, file = paste0( outdir, '/disagreements_w_c__protocols.tsv' ) )
