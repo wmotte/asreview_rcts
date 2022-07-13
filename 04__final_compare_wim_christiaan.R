725001ec2dfffa41d6e52ea4864b26cfb7ec349f
Wim Otte <wmotte@gmail.com>
2022-07-13 18:35:16 +0200

    Second round finished

--- 04__final_compare_wim_christiaan.R
+++ 04__final_compare_wim_christiaan.R
@@ -92,19 +92,19 @@ get_kappa_data <- function( type )
 outdir <- 'out.04.final.compare'
 dir.create( outdir, showWarnings = FALSE )
 
-#s_final <- get_kappa_data( type = 'systematic-reviews-only' )
+s_final <- get_kappa_data( type = 'systematic-reviews-only' )
 m_final <- get_kappa_data( type = 'meta-analysis-only' )
 r_final <- get_kappa_data( type = 'rodent-studies-only' )
 h_final <- get_kappa_data( type = 'human-studies-only' )
-#t_final <- get_kappa_data( type = 'trials-only' )
+t_final <- get_kappa_data( type = 'trials-only' )
 rt_final <- get_kappa_data( type = 'randomised-trials-only' )
 c_final <- get_kappa_data( type = 'children-only' )
 p_final <- get_kappa_data( type = 'protocols-only' )
 
 # combine
-res <- rbind( #s_final$res, 
+res <- rbind( s_final$res, 
                 m_final$res, r_final$res, h_final$res, 
-                #t_final$res, 
+                t_final$res, 
                 rt_final$res, c_final$res, p_final$res )
 
 # write to file
@@ -115,7 +115,7 @@ readr::write_tsv( res, file = paste0( outdir, '/kappas_w_c.tsv' ) )
 ##############################
 
 # 1) 
-#dim( disagreements_s <- s_final$out[ s_final$out$included_c != s_final$out$included_w, ] )
+dim( disagreements_s <- s_final$out[ s_final$out$included_c != s_final$out$included_w, ] )
 
 # 2) 
 dim( disagreements_m <- m_final$out[ m_final$out$included_c != m_final$out$included_w, ] )
@@ -127,7 +127,7 @@ dim( disagreements_r <- r_final$out[ r_final$out$included_c != r_final$out$inclu
 dim( disagreements_h <- h_final$out[ h_final$out$included_c != h_final$out$included_w, ] )
 
 # 5) 
-#dim( disagreements_t <- t_final$out[ t_final$out$included_c != t_final$out$included_w, ] )
+dim( disagreements_t <- t_final$out[ t_final$out$included_c != t_final$out$included_w, ] )
 
 # 6) 
 dim( disagreements_rt <- rt_final$out[ rt_final$out$included_c != rt_final$out$included_w, ] )
@@ -140,11 +140,11 @@ dim( disagreements_p <- p_final$out[ p_final$out$included_c != p_final$out$inclu
 
 
 # write to file
-#readr::write_tsv( disagreements_s, file = paste0( outdir, '/disagreements_w_c__systematic-reviews.tsv' ) )
+readr::write_tsv( disagreements_s, file = paste0( outdir, '/disagreements_w_c__systematic-reviews.tsv' ) )
 readr::write_tsv( disagreements_m, file = paste0( outdir, '/disagreements_w_c__meta-analysis.tsv' ) )
 readr::write_tsv( disagreements_r, file = paste0( outdir, '/disagreements_w_c__rodents.tsv' ) )
 readr::write_tsv( disagreements_h, file = paste0( outdir, '/disagreements_w_c__human.tsv' ) )
-#readr::write_tsv( disagreements_t, file = paste0( outdir, '/disagreements_w_c__trials.tsv' ) )
+readr::write_tsv( disagreements_t, file = paste0( outdir, '/disagreements_w_c__trials.tsv' ) )
 readr::write_tsv( disagreements_rt, file = paste0( outdir, '/disagreements_w_c__randomised-trials.tsv' ) )
 readr::write_tsv( disagreements_c, file = paste0( outdir, '/disagreements_w_c__child.tsv' ) )
 readr::write_tsv( disagreements_p, file = paste0( outdir, '/disagreements_w_c__protocols.tsv' ) )
